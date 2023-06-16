5b716ac81b4c323d5a343e1ac82bd13071186266
JoseAntonioBenitez <benitezruiz.joseantonio@gmail.com>
2023-06-16 18:49:56 +0200

    añadiendo menu

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSVC } from 'src/app/core/services/user.service';
 import { User } from 'src/app/core/models/user_model_gym';
+import { ScreenSizeSVCService } from 'src/app/core/services/screen-size-svc.service';
 
 @Component({
   selector: 'app-home',
@@ -10,8 +11,16 @@ import { User } from 'src/app/core/models/user_model_gym';
 export class HomePage implements OnInit {
 
   user:any;
+
+  ScreenSizeWidth:number = this.ScreenSizeSVC.getScreenSizeWidth();
+
+  //Size Plataforms
+  PhoneWidth:number = 500;
+  TabletWidth:number = 600;
+  MonitorWidth:number = 1000;
   constructor(
     protected userSVC:UserSVC,
+    private ScreenSizeSVC:ScreenSizeSVCService
 
   ) {
 
@@ -19,6 +28,24 @@ export class HomePage implements OnInit {
   ngOnInit(){
     const uid = this.userSVC.getUser().uid;
     this.user = this.userSVC.getUserByUid(uid)
+  }
+
+  getScreenSize(){
+    this.ScreenSizeWidth = this.ScreenSizeSVC.getScreenSizeWidth()
+  }
+
+  screenType():'BIG'|'SMALL'{
+    if (this.ScreenSizeWidth <= this.PhoneWidth){
+      return 'SMALL'
+    }else if (this.ScreenSizeWidth > this.TabletWidth && this.ScreenSizeWidth < this.MonitorWidth ){
+      return 'BIG'
+    }else{
+      return 'BIG';
+    }
+  }
+
+  signOut(){
+    this.userSVC.signOut();
   }
 
 }
